e04a3304fd994ea4e92a8ae7e90c734e1a8dd844
HenriqueNecrozmaG <wiket.moreira@gmail.com>
2026-09-04 22:25:20 -0300

    Alterações finais para a entrega

--- Challenge - How Are You/Assets/Scripts/Firebase/DatabaseManager.cs
+++ Challenge - How Are You/Assets/Scripts/Firebase/DatabaseManager.cs
@@ -1,21 +1,14 @@
 using Firebase.Database;
-using System;
-using System.Collections
-    ;
-using System.Xml.Linq;
 using UnityEngine;
 
 public class DatabaseManager : MonoBehaviour
 {
     private string userID;
-    private DatabaseReference dbReference;
+    private DatabaseReference dbReference = FirebaseDatabase.DefaultInstance.RootReference;
 
     void Start()
     {
         userID = SystemInfo.deviceUniqueIdentifier;
-        dbReference = FirebaseDatabase.DefaultInstance.RootReference;
-
-        GetInfo();
     }
 
     public void CreateUser()
@@ -24,44 +17,5 @@ public class DatabaseManager : MonoBehaviour
         string json = JsonUtility.ToJson(newUser);
 
         dbReference.Child("Users").Child(userID).SetRawJsonValueAsync(json);
-    }
-
-    void GetInfo()
-    {
-        StartCoroutine(GetInput((string lastPlayerInput) =>
-        {
-            CollaboratorsNew.lastPlayerInput = lastPlayerInput;
-        }));
-
-        StartCoroutine(GetScore((int score) =>
-        {
-            UIMinigame.score = score;
-        }));
-    }
-
-    public IEnumerator GetInput(Action<String> onCallback)
-    {
-        var userInputData = dbReference.Child("users").Child(userID).Child("lastPlayerInput").GetValueAsync();
-
-        yield return new WaitUntil(predicate: () => userInputData.IsCompleted);
-
-        if (userInputData != null)
-        {
-            DataSnapshot snapshot = userInputData.Result;
-            onCallback.Invoke((string)snapshot.Value);
-        }
-    }
-
-    public IEnumerator GetScore(Action<int> onCallback)
-    {
-        var userScoreData = dbReference.Child("users").Child(userID).Child("score").GetValueAsync();
-
-        yield return new WaitUntil(predicate: () => userScoreData.IsCompleted);
-
-        if (userScoreData != null)
-        {
-            DataSnapshot snapshot = userScoreData.Result;
-            onCallback.Invoke((int)snapshot.Value);
-        }
     }
 }
